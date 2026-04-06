afd06cc45fbdfec5de590cd37e9a31e4138031dc
Rushaan78 <rs2095@hw.ac.uk>
2026-04-06 20:52:20 +0400

    Updated firebase connection

--- Frontend/firebase_init.py
+++ Frontend/firebase_init.py
@@ -1,9 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
+import json
 
 #initializes firebase app and firestore client
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    try:
+        firebase_creds = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+        cred = credentials.Certificate(firebase_creds)
+    except:
+        cred = credentials.Certificate("firebase_key.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
